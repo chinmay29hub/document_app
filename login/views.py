8a40f915e09cc7bd9b3740c4282e395271ac8e9f
chinmay29hub <chinmaysonawane57@gmail.com>
2021-10-08 18:57:30 +0530

    pushed

--- login/views.py
+++ login/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control
 
+
+
+
+# Create your views here.
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def Login(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -37,9 +43,12 @@ def Register(request):
 
     return render(request, 'register.html')
 
+
+@login_required(login_url='/login')
 def Home(request):
     return render(request, 'home.html')
 
+@login_required
 def Logout(request):
     logout(request)
     messages.success(request, "Logged Out Succesfully")
